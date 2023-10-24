71459fdde250af709390adb93d90c05500888927
jleroux <lerouxjoachim@gmail.com>
2023-10-24 15:57:16 +0000

    Fix + beautify login page

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,67 +1,56 @@
 import React from 'react';
 import { CSSProperties } from 'react';
-import './App.css';
+import '@picocss/pico'
 
-type SeparatorProps = {
-	color?: string;
-	thickness?: number;
+const articleStyle: CSSProperties = {
+	width: '50%',
+	height: '50%',
+	position: 'absolute',
+	top: '50%',
+	left: '50%',
+	transform: 'translate(-50%, -50%)',
 };
 
-function Separator(props: SeparatorProps) {
-	const { color = 'black', thickness = 0.1 } = props;
-
-	const separatorStyle = {
-		width: '90%',
-		height: `${thickness}em`,
-		backgroundColor: color,
-		margin: '1rem 0',
-	};
-
-	return <div style={separatorStyle} />;
-}
-
-type CardProps = {
-	children: React.ReactNode;
+const buttonStyle: CSSProperties = {
+	width: '6rem',
+	margin: 'auto'
 };
-
-function Card(props: CardProps) {
-	const cardStyle: CSSProperties & { flexDirection: string } = {
-		backgroundColor: 'navy',
-		width: '32vw',
-		height: '42vh',
-		borderRadius: '42px',
-		// position: 'absolute',
-		// transform: 'translate(-50%, -50%)',
-		display: 'flex',
-		flexDirection: 'column',
-		justifyContent: 'center',
-		alignItems: 'center',
-	};
-
-	return (
-		<div style={cardStyle}>
-			{props.children}
-		</div>
-	);
-}
 
 // Renders a card form asking email and password, or login via 42 or Google
 function LoginPage() {
 	return (
-		<Card>
-			<form>
-				<label htmlFor="mail">Email:</label>
-				<input type="email" placeholder="Email" />
-				<label htmlFor="password">Password:</label>
-				<input type="password" placeholder="Password" />
-				<button type="submit">Login</button>
-			</form>
-			<Separator />
+		<article className="grid" style={articleStyle}>
 			<div>
-				<button>42</button>
-				<button>Google</button>
+				<hgroup>
+					<h1>Sign in</h1>
+					<h2>A minimalist layout for Login pages</h2>
+				</hgroup>
+				<form>
+					<input
+						type="email"
+						name="email"
+						placeholder="Email"
+						aria-label="Email"
+						autoComplete="email"
+						required
+					/>
+					<input
+						type="password"
+						name="password"
+						placeholder="Password"
+						aria-label="Password"
+						autoComplete="current-password"
+						required
+					/>
+					<button type="submit" className="contrast" onClick="event.preventDefault()">Login</button>
+				</form>
+				<progress value="0" max="100"></progress>
+				<div className='grid'>
+					<button style={{ ...buttonStyle, gridColumn: '1' }}>42</button>
+					<button style={{ ...buttonStyle, gridColumn: '2' }}>Google</button>
+				</div>
 			</div>
-		</Card>
+		</article>
 	);
 }
 
@@ -77,7 +66,7 @@ function HomePage() {
 export default function App() {
 	const logged = false;
 	return (
-		<div>
+		<div className='container'>
 			{logged ? <HomePage /> : <LoginPage />}
 		</div>
 	);
--- frontend/app/src/main.tsx
+++ frontend/app/src/main.tsx
@@ -1,6 +1,6 @@
-import { createRoot } from 'react-dom/client';
+import * as ReactDOMClient from 'react-dom/client';
 import App from './App';
 
-const container = document.getElementById('app');
-const root = createRoot(container!);
-root.render(<App/>);
+const container = document.getElementById('root');
+const root = ReactDOMClient.createRoot(container!);
+root.render(<App />);
