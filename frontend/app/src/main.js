3f812470ff47d39136aa6937335ee9d090874d32
Nit4r4 <87270463+Nit4r4@users.noreply.github.com>
2023-11-26 15:44:42 +0000

    login page avec menu lien ajax

---
+++ frontend/app/src/main.js
@@ -0,0 +1,8 @@
+// Votre code JavaScript, y compris la fonction userLoggedIn et les fonctions Ajax
+function userLoggedIn() {
+	// ... Logique de connexion ...
+	// Chargez la page de profil après la connexion
+	loadPage('profil');
+  }
+  
+  // Vos autres fonctions JavaScript et gestionnaires d'événements
