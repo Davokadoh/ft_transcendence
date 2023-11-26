2cb9bb7063ce68dd25ff7a1519b0b3fafaabe8f5
Nit4r4 <87270463+Nit4r4@users.noreply.github.com>
2023-11-26 14:49:20 +0100

    before merge

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,121 +1,123 @@
-import React from 'react';
-import { CSSProperties } from 'react';
-import '@picocss/pico'
-import './stylesheet.css'; //font Spoof styles
-import './App.css'; //CSS
+// import React from 'react';
+// import { CSSProperties } from 'react';
+// import '@picocss/pico'
+// import './stylesheet.css'; //font Spoof styles
+// import './App.css'; //CSS
 
-const articleStyle: CSSProperties = {
-	width: '40%',
-	height: '43%',
-	position: 'absolute',
-	top: '50%',
-	left: '50%',
-	transform: 'translate(-50%, -50%)',
-	backgroundColor: '#FFFFFF',
-	borderRadius: '30px',
-	// borderRadius: '10px 100px / 120px',
-};
-
-// const navbar: CSSProperties = {
-// 	float: left,
-// 	list-style-type: none,
-// 	margin: 0,
-// 	padding: 0,
-// 	overflow: hidden,
-// 	background-color: #333,
-// 	position: -webkit-sticky, /* Safari */
-// 	position: sticky,
-// 	top: 0,
+// const articleStyle: CSSProperties = {
+// 	width: '40%',
+// 	height: '43%',
+// 	position: 'relative',
+// 	margintop : '100%',
+// 	paddingtop : '10%',
+// 	top: '50%',
+// 	left: '50%',
+// 	transform: 'translate(-50%, -50%)',
+// 	backgroundColor: '#FFFFFF',
+// 	borderRadius: '30px',
+// 	// borderRadius: '10px 100px / 120px',
 // };
 
-const buttonStyle: CSSProperties = {
-	width: '6rem',
-	margin: 'auto',
-	color: '#9016B2'
-};
+// // const navbar: CSSProperties = {
+// // 	float: left,
+// // 	list-style-type: none,
+// // 	margin: 0,
+// // 	padding: 0,
+// // 	overflow: hidden,
+// // 	background-color: #333,
+// // 	position: -webkit-sticky, /* Safari */
+// // 	position: sticky,
+// // 	top: 0,
+// // };
 
-const titanOneFontStyles: CSSProperties = {
-	fontFamily: 'Titan One, cursive', // Use 'Titan One' font
-	fontWeight: 'normal', // Make it regular (not bold)
-	color: 'var(--vert)', // #87D300 variable dans index.html
-};
+// const buttonStyle: CSSProperties = {
+// 	width: '6rem',
+// 	margin: 'auto',
+// 	color: '#9016B2'
+// };
 
-const spoofBoldFontStyle: CSSProperties = {
-	fontFamily: 'Spoof, sans-serif',
-	fontWeight: 'bold', // Make it regular (not bold)
-	color: '#00BAB9'
-};
+// const titanOneFontStyles: CSSProperties = {
+// 	fontFamily: 'Titan One, cursive', // Use 'Titan One' font
+// 	fontWeight: 'normal', // Make it regular (not bold)
+// 	color: 'var(--vert)', // #87D300 variable dans index.html
+// };
 
-// Renders a card form asking email and password, or login via 42 or Google
-function LoginPage() {
-	return (
-		<article className="grid" style={articleStyle}>
-			<div>
-				<hgroup>
-				<h1 className="titan-one-font" style={titanOneFontStyles}>Welcome to the Pong-chos website ! </h1>
-					<h2 className='Spoof Bold' style={spoofBoldFontStyle}>A 42 project : Transcendence</h2>
-				</hgroup>
-				<form>
-					<input
-						type="email"
-						name="email"
-						placeholder="Email"
-						aria-label="Email"
-						autoComplete="email"
-						required
-					/>
-					<input
-						type="password"
-						name="password"
-						placeholder="Password"
-						aria-label="Password"
-						autoComplete="current-password"
-						required
-					/>
-					{/* <button type="submit" className="contrast" onClick="event.preventDefault()">Login</button> */}
-					<button type="submit" className="contrast" onClick={(event) => event.preventDefault()}>Login</button>
-				</form>
-				<progress value="0" max="100"></progress>
-				<div className='grid'>
-					<button className="spoof-bold-button" style={{
-						...buttonStyle, 
-						gridColumn: '1', 
-						backgroundColor: '#87D300',
-						border: '2px solid #ffffff',
-						}}>42</button>
-					<button className="spoof-bold-button" style={{ ...buttonStyle, 
-						gridColumn: '2',
-						backgroundColor: '#87D300',
-						border: '2px #FFFFFF'
-						}}>Google</button>
-				</div>
-			</div>
-		</article>
-	);
-}
+// const spoofBoldFontStyle: CSSProperties = {
+// 	fontFamily: 'Spoof, sans-serif',
+// 	fontWeight: 'bold', // Make it regular (not bold)
+// 	color: '#00BAB9'
+// };
 
-function HomePage() {
-	return (
-		<div>
-			Home Page
-		</div>
-	);
-}
-
-// Renders login page if user not logged in, otherwise renders home page
-export default function App() {
-	const logged = false;
-	return (
-		<div className='container'>
-			<LoginPage/>
-		</div>
-	);
-}
-
-// function Spoof() {
+// // Renders a card form asking email and password, or login via 42 or Google
+// function LoginPage() {
 // 	return (
-// 		<div style={spoofBoldFontStyle}>
-// 			<p>This text uses your custom font.</p>
+// 		<article className="grid" style={articleStyle}>
+// 			<div>
+// 				<hgroup>
+// 				<h1 className="titan-one-font" style={titanOneFontStyles}>Welcome to the Pong-chos website ! </h1>
+// 					<h2 className='Spoof Bold' style={spoofBoldFontStyle}>A 42 project : Transcendence</h2>
+// 				</hgroup>
+// 				<form>
+// 					<input
+// 						type="email"
+// 						name="email"
+// 						placeholder="Email"
+// 						aria-label="Email"
+// 						autoComplete="email"
+// 						required
+// 					/>
+// 					<input
+// 						type="password"
+// 						name="password"
+// 						placeholder="Password"
+// 						aria-label="Password"
+// 						autoComplete="current-password"
+// 						required
+// 					/>
+// 					{/* <button type="submit" className="contrast" onClick="event.preventDefault()">Login</button> */}
+// 					<button type="submit" className="contrast" onClick={(event) => event.preventDefault()}>Login</button>
+// 				</form>
+// 				<progress value="0" max="100"></progress>
+// 				<div className='grid'>
+// 					<button className="spoof-bold-button" style={{
+// 						...buttonStyle, 
+// 						gridColumn: '1', 
+// 						backgroundColor: '#87D300',
+// 						border: '2px solid #ffffff',
+// 						}}>42</button>
+// 					<button className="spoof-bold-button" style={{ ...buttonStyle, 
+// 						gridColumn: '2',
+// 						backgroundColor: '#87D300',
+// 						border: '2px #FFFFFF'
+// 						}}>Google</button>
+// 				</div>
+// 			</div>
+// 		</article>
+// 	);
+// }
+
+// function HomePage() {
+// 	return (
+// 		<div>
+// 			Home Page
 // 		</div>
 // 	);
 // }
+
+// // Renders login page if user not logged in, otherwise renders home page
+// export default function App() {
+// 	const logged = false;
+// 	return (
+// 		<div className='container'>
+// 			<LoginPage/>
+// 		</div>
+// 	);
+// }
+
+// // function Spoof() {
+// // 	return (
+// // 		<div style={spoofBoldFontStyle}>
+// // 			<p>This text uses your custom font.</p>
+// // 		</div>
+// // 	);
+// // }
