9aea654f20f37ee444653e2ec40fc14556244c2a
Nit4r4 <87270463+Nit4r4@users.noreply.github.com>
2023-12-07 11:25:23 +0000

    bande bleu info add && tsx removed

---
+++ frontend/app/src/main.jsx
@@ -0,0 +1,6 @@
+import * as ReactDOMClient from 'react-dom/client';
+import App from './App';
+
+const container = document.getElementById('root');
+const root = ReactDOMClient.createRoot(container!);
+root.render(<App />);
