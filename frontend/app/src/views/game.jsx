27c641c31257d5e98c3a2f74037c32fc52c15944
Nit4r4 <87270463+Nit4r4@users.noreply.github.com>
2023-12-26 11:50:29 +0000

    test space GAME boucing logo

--- frontend/app/src/views/game.jsx
+++ frontend/app/src/views/game.jsx
@@ -20,12 +20,13 @@ const game = (
 <br>
 <br>
 <br>
-<br>
 	<div class="container-fluid">
 	  <div class="row justify-content-center align-items-center">
 		<div class="col-lg-6">
 		  <div class="scrolling-text-container">
 		  <img src="./src/img/Pong-Chos_LOGO-min.png" alt="Scrolling Image" class="custom-image-spacing">		  </div>
+		 <br> 
+		 <br> 
 		  </div>
 		</div>
 	  </div>
