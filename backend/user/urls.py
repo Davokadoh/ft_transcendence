f9039c4625867956804a7104aabdd64c18741bd2
Joachim Leroux <lerouxjoachim@gmail.com>
2024-01-11 10:23:39 +0100

    Cleaner path for auth

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import login, callback
 
 urlpatterns = [
-    path("", login.as_view(), name="login"),
+    path("login", login.as_view(), name="login"),
     path("callback", callback.as_view(), name="callback"),
 ]
