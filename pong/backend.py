dac91755fe851f8b67ce669850df8d4224da6a48
Joachim Leroux <lerouxjoachim@gmail.com>
2024-01-16 16:06:50 +0100

    potential fix callback, need test w/ API

--- pong/backend.py
+++ pong/backend.py
@@ -16,7 +16,7 @@ class CustomAuthenticationBackend(BaseBackend):
         state = os.urandom(42)
         auth_url = "https://api.intra.42.fr/oauth/authorize?client_id={}&redirect_uri={}&scope={}&state={}&response_type=code".format(
             os.getenv("API_42_CLIENT_ID"),
-            "http://localhost:8000/auth/callback",
+            "http://localhost:8000/accounts/callback",
             "public",
             123,  # state
         )
@@ -24,37 +24,36 @@ class CustomAuthenticationBackend(BaseBackend):
 
     # Request 42API auth, return user, need to check state
     def callback(request):
-        if request.method == "GET":
-            code = request.data.get("code")
-            state = request.data.get("state")
-            response = requests.post(
-                "https://api.intra.42.fr/oauth/token",
-                data={
-                    "grant_type": "authorization_code",
-                    "client_id": os.getenv("API_42_CLIENT_ID"),
-                    "client_secret": os.getenv("API_42_CLIENT_SECRET"),
-                    "code": code,
-                    "redirect_uri": "http://localhost:8000/auth/callback",
-                    "state": state,
-                },
-            )
-            if not response.ok:
-                print("Error, status code is: " + response.status_code)
-                return None
-            access_token = response.json()["access_token"]
-            response = requests.get(
-                "https://api.intra.42.fr/v2/me",
-                headers={"Authorization": "Bearer " + access_token},
-            )
+        code = request.GET.get('code', '')
+        state = request.GET.get('state', '')
+        response = requests.post(
+            "https://api.intra.42.fr/oauth/token",
+            data={
+                "grant_type": "authorization_code",
+                "client_id": os.getenv("API_42_CLIENT_ID"),
+                "client_secret": os.getenv("API_42_CLIENT_SECRET"),
+                "code": code,
+                "redirect_uri": "http://localhost:8000/accounts/callback",
+                "state": state,
+            },
+        )
+        if not response.ok:
+            print("Error, status code is: " + response.status_code)
+            return None
+        access_token = response.json()["access_token"]
+        response = requests.get(
+            "https://api.intra.42.fr/v2/me",
+            headers={"Authorization": "Bearer " + access_token},
+        )
 
-            try:
-                user = User.objects.get(id=response.json()["id"])
-            except User.DoesNotExist:
-                user = User.objects.create_user(response.json())
+        try:
+            user = User.objects.get(id=response.json()["id"])
+        except User.DoesNotExist:
+            user = User.objects.create_user(response.json())
 
-            user.access_token.refresh(access_token)
+        user.access_token.refresh(access_token)
 
-            return user
+        return user
 
     def get_user(self, request, **kwargs):
         if True:
