2c25076439007d25535210a64fb276e59f6f7719
Joachim Leroux <lerouxjoachim@gmail.com>
2024-01-16 13:14:50 +0100

    move all jsx to html django templates

--- pong/urls.py
+++ pong/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("home", views.home, name="home"),
-    path("game", views.game, name="game"),
     path("login", views.login, name="login"),
     path("callback", views.callback, name="callback"),
-    path("user", views.user, name="user"),
-    path("user/<int:num>", views.user, name="user"),
+    path("home", views.home, name="home"),
+    path("game", views.game, name="game"),
+    path("chat", views.chat, name="chat"),
+    path("profil", views.profil, name="profil"),
 ]
