de4801a1f9c71bdb443864b1fa1f6fe014ebb7a8
jud42 <judiismals@gmail.com>
2024-01-18 11:31:15 +0100

    upd

---
+++ frontend/app/src/CHAT/temp.js
@@ -0,0 +1,10 @@
+// toogle
+const chatboxToogle = document.querySelector()
+const chatboxMessage = document.querySelector()
+
+chatboxToogle.addEventListner('click', function () {
+  chatboxMessage.
+
+})
+
+// toogle channel
