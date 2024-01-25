60492ddecc9fe97c4510e35e5bdab4ce163b5281
Nit4r4 <87270463+Nit4r4@users.noreply.github.com>
2024-01-25 10:48:19 +0100

    ok

--- frontend/app/src/scripts/profil.js
+++ frontend/app/src/scripts/profil.js
@@ -61,48 +61,6 @@ $(document).ready(function () {
 			// Fermer la modal
 			$('#setModal').modal('hide');
 		});
-	
-		
-	// Initialiser les info-bulles non utiliser normallement - a supprimer
-	// var tooltips = new bootstrap.Tooltip(document.body, {
-	// 	selector: '[data-toggle="tooltip"]'
-	// });
-
-
-	console.log('Script modif Image chargé avec succès !');
-	// Fonction pour gérer le téléchargement d'image
-	$('#modifyImageButton').on('click', function () {
-		$('#imageInput').click();
-	});
-
-	$('#imageInput').on('change', function (event) {
-		const fileInput = event.target;
-		const profileImage = $('#profileImage');
-
-		const file = fileInput.files[0];
-		if (file) {
-			const reader = new FileReader();
-			reader.onload = function (e) {
-				profileImage.attr('src', e.target.result);
-			};
-			reader.readAsDataURL(file);
-		}
-	});
-
-	// Fonction pour gérer la modification du nom d'utilisateur
-	$('#username').on('blur', function () {
-		console.log('Script fin chargé avec succès !');
-		const usernameInput = $('#username');
-		const newUsername = usernameInput.val().trim();
-		if (newUsername !== '') {
-			// Vous pouvez envoyer newUsername à votre serveur ou API pour la mise à jour
-			console.log('Nouveau nom d\'utilisateur :', newUsername);
-		} else {
-			// Réinitialisez la valeur si elle est vide
-			usernameInput.val('John Doe');
-		}
-
-	});
 
 
 // Placer ce code à la fin de : profil.js pour enregistrer l image et le nom d utilisateur
