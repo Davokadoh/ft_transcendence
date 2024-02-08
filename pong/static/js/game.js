115be007af50e8c50a99a32e2a92a4b3543d37ce
Claire Reyt <clairereyt@macbook-pro-de-claire.home>
2024-02-08 12:06:13 +0100

    moves don'twork

--- pong/static/js/game.js
+++ pong/static/js/game.js
@@ -3,7 +3,10 @@ window.addEventListener("StartGameEvent", () => {
 		console.log("TEST");
 		
 		StartGame();
+		playGame();
 	});
+
+	canvas.addEventListener('mousemove', playerMove);
 });
 
 
@@ -29,11 +32,16 @@ export function StartGame() {
 			x: canvas.width / 2,
 			y: canvas.height / 2,
 			r: 5,
+			speed:{
+				x: 2,
+				y: 2,
+			}
 		},
 
 	};
 
-	console.log("salut");
+	// Draw field
+
 	ctx.fillStyle = 'black';
 	ctx.fillRect(0, 0, canvas.width, canvas.height); // dessine un grand carre noir ou pas (la taille est geree par le html)
 
@@ -46,20 +54,54 @@ export function StartGame() {
 	ctx.stroke();
 
 	// Draw players
-	// ctx.fillStyle = 'white';
-	// ctx.fillRect(0, 0, playerWidth, playerHeight);
-	// ctx.moveTo(canvas.width - playerWidth, 0);
-	// ctx.fillRect(canvas.width - playerWidth, canvas.height - playerHeight, playerWidth, playerHeight);
-	// ctx.fillRect(canvas.width - playerWidth, play.computer.y, playerWidth, playerHeight);
+
 	ctx.fillStyle = '#9016B2';
 	ctx.fillRect(0, play.player.y, playerWidth, playerHeight);
 	ctx.moveTo(canvas.width - playerWidth, 0);
 	ctx.fillRect(canvas.width - playerWidth, play.computer.y, playerWidth, playerHeight);
 	ctx.fill();
 	
+	// Draw ball
+
 	ctx.beginPath();
 	ctx.fillStyle = '#87d300';
 	ctx.arc(play.ball.x, play.ball.y, play.ball.r, 0, Math.PI * 2, false);
 	ctx.fill();
 
+};
+
+// function ballMove() {
+//     // Rebounds on top and bottom
+//     if (game.ball.y > canvas.height || game.ball.y < 0) {
+//         game.ball.speed.y *= -1;
+//     }
+
+//     if (game.ball.x > canvas.width - PLAYER_WIDTH) {
+//         collide(game.computer);
+//     } else if (game.ball.x < PLAYER_WIDTH) {
+//         collide(game.player);
+//     }
+// 	game.ball.x += game.ball.speed.x;
+//     game.ball.y += game.ball.speed.y;
+// }
+
+export function playerMove(event){
+
+	var canvasLocation = canvas.getBoundingClientRect();
+	var mouseLocation = event.clientY - canvasLocation.y;
+
+	console.log("playerMove fonction");
+
+	play.player.y = mouseLocation - playerHeight / 2;
+};
+
+export function playGame() {
+
+	game.ball.x += game.ball.speed.x;
+    game.ball.y += game.ball.speed.y;
+
+    StartGame();
+	playGame();
+
+    requestAnimationFrame(playGame);
 };
