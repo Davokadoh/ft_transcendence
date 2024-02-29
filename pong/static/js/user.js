348d99ad91d4143431363442509aa9fe2ffabc73
Claire Reyt <clairereyt@gmail.com>
2024-02-29 12:30:54 +0000

    error in user fixed

--- pong/static/js/user.js
+++ pong/static/js/user.js
@@ -12,10 +12,12 @@ export function user() { //modif de claire du 26.092.24 pour regler le soucis d'
     // let user = document.getElementById('user');
     let profilPicture = document.getElementById('profil-picture');
     let searched_username = document.getElementById('searchInput');
-    searched_username.onchange = function() {
-        console.log("searched_username JS = ", searched_username.value);
-        // console.log("WINDOW HREF = ", window.location.href);
-        // user.href = `/user/${searched_username.value}/`;
-        profilPicture.href = `/user/${searchedUsername.value}/`;
-    };
+    if (searched_username){
+        searched_username.onchange = function() {
+            console.log("searched_username JS = ", searched_username.value);
+            // console.log("WINDOW HREF = ", window.location.href);
+            // user.href = `/user/${searched_username.value}/`;
+            profilPicture.href = `/user/${searchedUsername.value}/`;
+        };
+    }
 }
