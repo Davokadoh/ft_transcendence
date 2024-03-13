c03d4cda561fe6691ef955b866176ee87b4fbffa
Joachim Leroux <lerouxjoachim@gmail.com>
2024-03-13 18:16:03 +0100

    Fix serving index.js twice, fix websockets

--- django/ftt/settings.py
+++ django/ftt/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ROOT_URLCONF = "ftt.urls"
 
