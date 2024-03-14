65c301ac6601c8889569766e67e7b25a0d0e6c05
Nit4r4 <87270463+Nit4r4@users.noreply.github.com>
2024-03-14 13:25:18 +0100

    CSS ok settings.py ligne 146

--- django/ftt/settings.py
+++ django/ftt/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'tmp/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
